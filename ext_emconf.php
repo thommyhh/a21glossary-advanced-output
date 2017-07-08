67e6c91746a44a3c758f313d4fa88f8ca6f24bde
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2017-07-08 13:18:50 +0200

    [TASK] Import of version 0.2.1

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "a21glossary_advanced_output".
+ *
+ * Auto generated 20-09-2015 15:29
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'A21glossary Advanced Output',
+	'description' => 'A extended Output for the A21glossary. SEO friendly incl. RealURL support & search function',
+	'category' => 'fe',
+	'shy' => false,
+	'version' => '0.2.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => NULL,
+	'loadOrder' => NULL,
+	'module' => NULL,
+	'state' => 'beta',
+	'uploadfolder' => false,
+	'createDirs' => NULL,
+	'modify_tables' => NULL,
+	'clearcacheonload' => false,
+	'lockType' => NULL,
+	'author' => 'Tim Lochmueller, Thomas Buss',
+	'author_email' => 'webmaster@fruit-lab.de',
+	'author_company' => 'www.fruit-lab.de',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:42:"class.tx_a21glossaryadvancedoutput_eid.php";s:4:"a877";s:12:"ext_icon.gif";s:4:"9231";s:17:"ext_localconf.php";s:4:"4237";s:14:"ext_tables.php";s:4:"1491";s:16:"locallang_db.xml";s:4:"2189";s:4:"pi1/";s:4:"d41d";s:36:"pi1/a21glossary_advanced_output.html";s:4:"9f04";s:46:"pi1/class.tx_a21glossaryadvancedoutput_pi1.php";s:4:"529c";s:17:"pi1/locallang.xml";s:4:"0bb4";s:51:"pi1/a21glossary_advanced_output_indexed_search.html";s:4:"eca2";s:4:"res/";s:4:"d41d";s:30:"res/jquery.autocomplete.min.js";s:4:"a30f";s:27:"res/jquery.autocomplete.css";s:4:"719c";s:7:"static/";s:4:"d41d";s:20:"static/constants.txt";s:4:"7436";s:16:"static/setup.txt";s:4:"cdec";}',
+);
+
+?>
